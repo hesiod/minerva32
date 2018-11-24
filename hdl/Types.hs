5e77c16882bebc745820a5dedd81276c06c683f4
Tobias Markus <tobias@miglix.eu>
2018-11-24 18:35:47 +0100

    Add Undefind instances

--- hdl/Types.hs
+++ hdl/Types.hs
@@ -8,28 +8,30 @@ import Control.DeepSeq (NFData)
 import Clash.Prelude
 import Data.Default.Class (Default(..))
 
-data InstrType = R | I | S | B | U | J | Z deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+data InstrType = R | I | S | B | U | J | Z deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
-data Opcode = LOAD | STORE | LUI | AUIPC | JAL | JALR | BRANCH | IARITH | ARITH | INVALID deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+data Opcode = LOAD | STORE | LUI | AUIPC | JAL | JALR | BRANCH | IARITH | ARITH | INVALID deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
-data Width = Byte | HalfWord | Word deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+data Width = Byte | HalfWord | Word deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
---data AluOp = ADD | SUB | SLL | SLT | SLTU | XOR | OR | AND | SRL | SRA deriving (Eq, Show, Generic, NFData, Lift, ShowX)
-data AluOp = Add | Sub | Sll | Slt | Sltu | Xor | Or | And | Srl | Sra deriving (Eq, Show, Generic, NFData, Lift, ShowX)
-data AluSrc = Zero | Rs | Imm12 | Imm20 | OffImm12 | OffImm20 | Pc deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+--data AluOp = ADD | SUB | SLL | SLT | SLTU | XOR | OR | AND | SRL | SRA deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
+data AluOp = Add | Sub | Sll | Slt | Sltu | Xor | Or | And | Srl | Sra deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
+data AluSrc = Zero | Rs | Imm12 | Imm20 | OffImm12 | OffImm20 | Pc deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
-data JumpType = Jump | Branch BranchType deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+data JumpType = Jump | Branch BranchType deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
-data WritebackSrc = WbPc | WbAluRes | WbMem deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+data WritebackSrc = WbPc | WbAluRes | WbMem deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
-data BranchType = B_EQ | B_NE | B_LT | B_GE | B_LTU | B_GEU deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+data BranchType = B_EQ | B_NE | B_LT | B_GE | B_LTU | B_GEU deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
-data MemoryRequest = MemWrite | MemRead deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+data MemoryRequest = MemWrite | MemRead deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
 type RegisterIndex = Index 32
 type MWord = BitVector 32
 
-newtype Instruction = Instruction (BitVector 32) deriving (Generic, NFData, Lift, ShowX)
+type DoJump = Maybe MWord
+
+newtype Instruction = Instruction (BitVector 32) deriving (Generic, NFData, Lift, ShowX, Undefined)
 
 data InterInstr = InterInstr {
     opcode :: Opcode,
@@ -39,7 +41,7 @@ data InterInstr = InterInstr {
     rs1 :: RegisterIndex,
     rs2 :: RegisterIndex,
     rd :: RegisterIndex
-    } deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+    } deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
 instance Default InterInstr where
     def = InterInstr INVALID zeroBits zeroBits zeroBits minBound minBound minBound
@@ -48,7 +50,7 @@ data AluCtrl = AluCtrl {
     aluOp :: AluOp,
     aluSrc1 :: AluSrc,
     aluSrc2 :: AluSrc
-    } deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+    } deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
 instance Default AluCtrl where
     def = AluCtrl Add Zero Zero
@@ -60,7 +62,7 @@ data InstrDescr = InstrDescr {
     writebackSrc :: Maybe WritebackSrc,
     memoryRequest :: Maybe MemoryRequest,
     pc :: MWord
-    } deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+    } deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 
 instance Default InstrDescr where
     def = InstrDescr def def Nothing Nothing Nothing def
@@ -70,22 +72,40 @@ newtype RegisterFile = RegisterFile (Vec 32 MWord)
 data FetchResults = FetchResults {
     instruction :: Instruction,
     fetchedPc :: MWord
-    } deriving (Generic, NFData, Lift, ShowX)
+    } deriving (Generic, NFData, Lift, ShowX, Undefined)
 
 --data DecodeResults = DecodeResults {}
 
 data ExecuteResults = ExecuteResults {
-    aluRes :: MWord,
-    doJump :: Bit
-    } deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+    erInstrDescr :: InstrDescr,
+    erAluRes :: MWord,
+    erDoJump :: DoJump
+    } deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 instance Default ExecuteResults where
-    def = ExecuteResults zeroBits 0
+    def = ExecuteResults def zeroBits Nothing
+
+data MemoryResults = MemoryResults {
+    mrInstrDescr :: InstrDescr,
+    mrAluRes :: MWord,
+    mrMemRes :: MWord
+    } deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
+instance Default MemoryResults where
+    def = MemoryResults def zeroBits zeroBits
 
 data ForwardRequest = ForwardRequest {
     req_rs1 :: RegisterIndex,
     req_rs2 :: RegisterIndex
-    } deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+    } deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
 data ForwardResponse = ForwardResponse {
     vrs1 :: MWord,
     vrs2 :: MWord
-    } deriving (Eq, Show, Generic, NFData, Lift, ShowX)
+    } deriving (Eq, Show, Generic, NFData, Lift, ShowX, Undefined)
+
+data CacheRequest = CacheRequest {
+    iaddr :: MWord,
+    daddr :: MWord,
+    drequest :: Maybe MemoryRequest
+    }
+
+type SPAddr = Unsigned 14
+type SPData = BitVector 32
